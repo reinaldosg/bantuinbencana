54ea4c8bc1105f3c9f2f8236fcaa84c369bb7017
feliciafunay <felicia.funay@gmail.com>
2020-04-09 23:11:58 +0700

    membuat profile UI dengan fragment

--- app/src/main/java/dsc/machung/bantuanbencana/MainActivity.java
+++ app/src/main/java/dsc/machung/bantuanbencana/MainActivity.java
@@ -1,45 +1,40 @@
 package dsc.machung.bantuanbencana;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    Animation ttb;
-    Animation btt;
-    ImageView logo;
-    EditText username;
-    EditText password;
-    CheckBox rememberme;
-    Button login;
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-//        ttb = AnimationUtils.loadAnimation(this, R.anim.ttb);
-//        btt = AnimationUtils.loadAnimation(this, R.anim.btt);
-//
-//        logo = findViewById(R.id.logo);
-//        username = findViewById(R.id.username);
-//        password = findViewById(R.id.password);
-//        rememberme = findViewById(R.id.rememberme);
-//        login = findViewById(R.id.login);
-//
-//        //run animation
-//        logo.startAnimation(ttb);
-//        username.startAnimation(btt);
-//        password.startAnimation(btt);
-//        rememberme.startAnimation(btt);
-//        login.startAnimation(btt);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Fragment fragment;
+        fragment = new BlankFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.commit();
     }
 }
